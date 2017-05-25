9ca1211e57a0a988c10f8f9ac19c7f44e27f2fc8
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-05-25 17:28:37 +0300

    - changed person program - added warning log and age check

--- src/main/java/Portfolio/PeoplePortfolio.java
+++ src/main/java/Portfolio/PeoplePortfolio.java
@@ -1,8 +1,11 @@
 package Portfolio;
 import java.util.Scanner;
+import java.util.logging.*;
+
 
 public class PeoplePortfolio {
 	private static Scanner in;
+	
 
 	public static void main(String[] args) {
 		
@@ -21,8 +24,19 @@ public class PeoplePortfolio {
 		in = new Scanner(System.in);
 		String email1 = in.next();
 		
-		Person person1 = new Person(name1, surname1, email1); 
-		//we don't need setters here as we used a constructor with parameters
+		System.out.print("Please type 1-st person hobby");
+		System.out.println();
+		in = new Scanner(System.in);
+		String hobby1 = in.next();
+		
+		System.out.print("Please type 1-st person age");
+		System.out.println();
+		in = new Scanner(System.in);
+		int age1 = in.nextInt();
+				
+		Person person1 = new Person(name1, surname1, email1, hobby1, age1); 
+		
+		person1.checkAge(age1);
 		
 		System.out.println("First person:");
 		//used printData method as per description
@@ -43,15 +57,101 @@ public class PeoplePortfolio {
 		in = new Scanner(System.in);
 		String email2 = in.next();
 		
+		System.out.print("Please type 2-nd person hobby");
+		System.out.println();
+		in = new Scanner(System.in);
+		String hobby2 = in.next();
+		
+		System.out.print("Please type 2-nd person age");
+		System.out.println();
+		in = new Scanner(System.in);
+		int age2 = in.nextInt();
+		
+		
+		
 		Person person2 = new Person(); 
 		//I used a default constructor in this case so I need setters
 		
 		person2.setName(name2);
 		person2.setSurname(surname2);
 		person2.setEmail(email2);
+		person2.setAge(age2);
+		person2.setHobby(hobby2);
+		
+		person1.checkAge(age2);
 		
 		System.out.println("Second person:");
 		person2.pritnData();
+		
+		System.out.print("Please type 3-d person name");
+		System.out.println();
+		in = new Scanner(System.in);
+		String name3 = in.next();
+		
+		System.out.print("Please type 3-d person surname");
+		System.out.println();
+		in = new Scanner(System.in);
+		String surname3 = in.next();
+		
+		System.out.print("Please type 3-d person email");
+		System.out.println();
+		in = new Scanner(System.in);
+		String email3 = in.next();
+		
+		System.out.print("Please type 3-d person hobby");
+		System.out.println();
+		in = new Scanner(System.in);
+		String hobby3 = in.next();
+		
+		System.out.print("Please type 3-d person age");
+		System.out.println();
+		in = new Scanner(System.in);
+		int age3 = in.nextInt();
+		
+		
+		Person person3 = new Person(name3, surname3, email3, hobby3, age3); 
+		//we don't need setters here as we used a constructor with parameters
+		
+		person1.checkAge(age3);
+		
+		System.out.println("Third person:");
+		//used printData method as per description
+		person3.pritnData();
+		
+		System.out.print("Please type 4-th person name");
+		System.out.println();
+		in = new Scanner(System.in);
+		String name4 = in.next();
+		
+		System.out.print("Please type 4-th person surname");
+		System.out.println();
+		in = new Scanner(System.in);
+		String surname4 = in.next();
+		
+		System.out.print("Please type 4-th person email");
+		System.out.println();
+		in = new Scanner(System.in);
+		String email4 = in.next();
+		
+		System.out.print("Please type 4-th person hobby");
+		System.out.println();
+		in = new Scanner(System.in);
+		String hobby4 = in.next();
+		
+		System.out.print("Please type 4-th person age");
+		System.out.println();
+		in = new Scanner(System.in);
+		int age4 = in.nextInt();
+		
+		
+		Person person4 = new Person(name4, surname4, email4, hobby4, age4); 
+		//we don't need setters here as we used a constructor with parameters
+		
+		person1.checkAge(age4);
+		
+		System.out.println("First person:");
+		//used printData method as per description
+		person4.pritnData();
 		
 		
 	}
--- src/main/java/Portfolio/Person.java
+++ src/main/java/Portfolio/Person.java
@@ -1,18 +1,27 @@
 package Portfolio;
+import java.io.IOException;
+import java.util.logging.*;
 
 public class Person {
 	private String name;
 	private String surname;
 	private String email;
+	private int age;
+	private String hobby;
+	private static Logger LOGGER = Logger.getLogger(PeoplePortfolio.class.getName());
 	
 	Person (){
 
 	}
 	
-	Person (String n, String s, String e){
+	Person (String n, String s, String e, String h, int a){
 		name = n;
 		surname = s;
 		email = e;
+		hobby = h;
+		age = a;
+		
+		
 	}
 
 	public void setName(String inputName){
@@ -30,6 +39,18 @@ public class Person {
 		this.email = inputEmail;
 		
 	}
+	
+	public void setAge(int inputAge){
+		
+		this.age = inputAge;
+		
+	}
+
+	public void setHobby(String inputHobby){
+	
+	this.hobby = inputHobby;
+	
+}
 	public String getName(){
 		
 		return this.name;
@@ -46,10 +67,31 @@ public class Person {
 		
 	}
 	
+	public int getAge(){
+		
+		return this.age;
+		
+	}
+
+	public String getHobby(){
+	
+	return this.hobby;
+	
+	}
+	public void checkAge (int age) {
+		
+	if (age < 1){
+		
+		LOGGER.warning("User has entered incorrect age. Age cannot be less that 1");
+		}
+	}
+	
 	public void pritnData(){
 		System.out.println("Name is: " + getName());
 		System.out.println("Surname is: " + getSurname());
 		System.out.println("Email is: " + getEmail());
+		System.out.println("Age is: " + getAge());
+		System.out.println("Hobby is: " + getHobby());
 	}
 
 }
